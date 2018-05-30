f0173def2bc803442e2db85dc85fba6747600857
Walter Proulx <Champtech34@champlain.edu>
2018-05-30 08:56:50 -0400

    Adding security to files.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,54 +1,57 @@
 @extends('layouts.master')
 
 @section('content')
-  <h1 class="title">Register a User</h1>
 
-  <div class="form-card">
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+  @if (Auth::check())
+    <h1 class="title">Register a User</h1>
 
-        <label for="name">{{ __('Name') }}</label>
-        <div>
-            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+    <div class="form-card">
+      <form method="POST" action="{{ route('register') }}">
+          @csrf
 
-            @if ($errors->has('name'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('name') }}</strong>
-                </span>
-            @endif
-        </div>
+          <label for="name">{{ __('Name') }}</label>
+          <div>
+              <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
-        <label for="email">{{ __('E-Mail Address') }}</label>
+              @if ($errors->has('name'))
+                  <span class="invalid-feedback">
+                      <strong>{{ $errors->first('name') }}</strong>
+                  </span>
+              @endif
+          </div>
 
-        <div>
-            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+          <label for="email">{{ __('E-Mail Address') }}</label>
 
-            @if ($errors->has('email'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-            @endif
-        </div>
+          <div>
+              <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-        <label for="password">{{ __('Password') }}</label>
-        <div>
-            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+              @if ($errors->has('email'))
+                  <span class="invalid-feedback">
+                      <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+              @endif
+          </div>
 
-            @if ($errors->has('password'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
-        </div>
+          <label for="password">{{ __('Password') }}</label>
+          <div>
+              <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-        <div>
-            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-        </div>
+              @if ($errors->has('password'))
+                  <span class="invalid-feedback">
+                      <strong>{{ $errors->first('password') }}</strong>
+                  </span>
+              @endif
+          </div>
 
-        <button type="submit" class="btn btn-primary">
-            {{ __('Register') }}
-        </button>
-    </form>
-  </div>
+          <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+          <div>
+              <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+          </div>
+
+          <button type="submit" class="btn btn-primary">
+              {{ __('Register') }}
+          </button>
+      </form>
+    </div>
+  @endif
 @endsection
