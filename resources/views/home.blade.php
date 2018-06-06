1440270913ecc566d959d5da6c82f067afeabd9a
Walter Proulx <Champtech34@champlain.edu>
2018-06-06 14:57:06 -0400

    Quick change to homepage.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,33 +7,27 @@
   <div class="home-row">
     <div class="home-card">
       <a href="https://servicedesk.champlain.edu" class="home-card-title" target="_blank">ServiceDesk</a>
-      Text
     </div>
     <div class="home-card">
       <a href="https://my.champlain.edu/ad" class="home-card-title" target="_blank">Active Directory</a>
-      Text
     </div>
   </div>
 
   <div class="home-row">
     <div class="home-card">
       <a href="https://welcome.champlain.edu/showLogin.cc" class="home-card-title" target="_blank">Champlain Welcome</a>
-      Text
     </div>
     <div class="home-card">
       <a href="https://my.champlain.edu/" class="home-card-title" target="_blank">My Champlain</a>
-      Text
     </div>
   </div>
 
   <div class="home-row">
     <div class="home-card">
       <a href="/document_general/29" class="home-card-title">Phone Extensions</a>
-      Text
     </div>
     <div class="home-card">
       <a href="#" class="home-card-title">Department Contact</a>
-      Text
     </div>
   </div>
 @endsection
